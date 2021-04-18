07fcca28ee573d22f686237843c462cb072491a7
oereo <dlstpgns0406@gmail.com>
2021-04-18 15:05:44 +0900

    feat: add input_lotto_total_amount method

---
+++ python-lotto/ui/input_view.py
@@ -0,0 +1,42 @@
+from lotto.lotto import LottoInformation
+from lotto_decorator.printer_decorator import printer_format
+
+
+class InputMessage:
+    INPUT_PURCHASED_LOTTO_TOTAL_AMOUNT_MESSAGE = '로또 구입 금액을 입력해주세요: '
+    INPUT_INVALID_FORMAT_LOTTO_TOTAL_AMOUNT_MESSAGE = '숫자형식의 금액을 입력해주세요.'
+    INPUT_NOT_ZERO_LOTTO_TOTAL_AMOUNT_MESSAGE = '구입할 로또 금액으로 0원을 입력하실 수 없습니다. 로또 한장 이상의 금액을 입력해주세요.'
+    INPUT_NOT_SATISFY_MINIMUM_LOTTO_TOTAL_AMOUNT_MESSAGE = "로또 한장 이상의 금액을 입력해주세요."
+
+
+class NotSatisfyMinimumAmountError(Exception):
+    def __init__(self):
+        super().__init__(InputMessage.INPUT_NOT_SATISFY_MINIMUM_LOTTO_TOTAL_AMOUNT_MESSAGE)
+
+
+class NotZeroLottoAmountError(Exception):
+    def __init__(self):
+        super().__init__(InputMessage.INPUT_NOT_ZERO_LOTTO_TOTAL_AMOUNT_MESSAGE)
+
+
+@printer_format
+def input_purchased_lotto_total_amount():
+    while True:
+        try:
+            lotto_total_amount = int(input(InputMessage.INPUT_PURCHASED_LOTTO_TOTAL_AMOUNT_MESSAGE))
+            if is_over_minimum_lotto_total_amount(lotto_total_amount):
+                return lotto_total_amount
+        except ValueError:
+            print(InputMessage.INPUT_INVALID_FORMAT_LOTTO_TOTAL_AMOUNT_MESSAGE)
+        except Exception as e:
+            print(e)
+
+
+def is_over_minimum_lotto_total_amount(lotto_total_amount):
+    if lotto_total_amount == 0:
+        raise NotZeroLottoAmountError
+
+    if lotto_total_amount < LottoInformation.LOTTO_PRICE:
+        raise NotSatisfyMinimumAmountError
+
+    return True
